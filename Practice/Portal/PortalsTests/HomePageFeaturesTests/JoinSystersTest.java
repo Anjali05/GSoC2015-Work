982da63866ea085fccc36df39752b2f8bf9bb7a5
Anjali05 <shally.hp5@gmail.com>
2015-06-07 00:01:03 +0530

    Portal HomeFeature Tests modified

---
+++ Practice/Portal/PortalsTests/HomePageFeaturesTests/JoinSystersTest.java
@@ -0,0 +1,15 @@
+package HomePageFeaturesTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JoinSystersTest {
+
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
