add3d13b86d67d9c112b39e4ef3475fc04540ccd
Anjali05 <shally.hp5@gmail.com>
2015-06-12 11:07:20 +0530

    Parameterised test for login added

--- Practice/Portal/PortalsTests/LoginTests/LoginTest.java
+++ Practice/Portal/PortalsTests/LoginTests/LoginTest.java
@@ -1,24 +1,47 @@
 package LoginTests;
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
+
 import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
 import BaseTests.CommonTests;
 import Login.LoggedInCheck;
 import Login.LoginPage;
 
-
+@RunWith(Parameterized.class)
 public class LoginTest extends CommonTests
 {
 	static String s="Login";
-	public LoginTest() 
+	private String username;
+	private String password;
+	
+	@Parameters
+	public static Collection<Object[]>data()
+	{
+		return Arrays.asList(new Object[][]{
+				{"anjali","anjali"},
+				{"abc","xyz"},
+				{"random",""}
+		});		
+	}
+	public LoginTest(String username, String password) 
 	{
 		super(s);
+		this.username=username;
+		this.password=password;
+		
 	}
 
 	@Test
 	public void test() 
 	{
 		LoginPage.Goto();
-		LoginPage.LoginAs("Anjali").WithPassword("anjali").Login();
+		LoginPage.LoginAs(username).WithPassword(password).Login();
 		Assert.assertEquals("Failed",true,LoggedInCheck.IsAt());
 	}
 	
