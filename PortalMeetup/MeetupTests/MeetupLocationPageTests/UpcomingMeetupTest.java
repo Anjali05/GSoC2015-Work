c710ea262aaf35fc7cf86c15a0645f4e869cfbc6
Anjali05 <shally.hp5@gmail.com>
2015-06-17 20:35:59 +0530

    Generic files added for meetup

---
+++ PortalMeetup/MeetupTests/MeetupLocationPageTests/UpcomingMeetupTest.java
@@ -0,0 +1,19 @@
+package MeetupLocationPageTests;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Test;
+
+/*
+ * @Copywrite Systers, an Anita Borg Institute Community
+ * @author Anjali
+ */
+
+public class UpcomingMeetupTest {
+
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
